d3d059a79aa1ae4ef5d7502888d5cbf4ffa30928
oktambek2003 <116434960+oktambek2003@users.noreply.github.com>
2023-07-22 18:56:06 +0500

    oz qoldi

---
+++ lib/audio_detail.dart
@@ -0,0 +1,294 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class AudiaPlayer_Page extends StatefulWidget {
+  const AudiaPlayer_Page({super.key});
+
+  @override
+  State<AudiaPlayer_Page> createState() => _AudiaPlayer_PageState();
+}
+
+class _AudiaPlayer_PageState extends State<AudiaPlayer_Page> {
+  int i = 0;
+  double secons = 0;
+  late Color color;
+  late List Books;
+  ReadData() async {
+    await DefaultAssetBundle.of(context)
+        .loadString('json/books.json')
+        .then((value) {
+      setState(() {
+        Books = json.decode(value);
+      });
+    });
+  }
+
+  final player = AudioPlayer();
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+  bool isplaying = false;
+  bool ispaused = false;
+  bool isLoop = false;
+  bool isRepeat = false;
+  List<IconData> _icons = [Icons.play_circle_fill, Icons.pause_circle_filled];
+  String formatTime(int second) {
+    return "${(Duration(seconds: second))}".split(".")[0].padLeft(8, "0");
+  }
+
+  @override
+  void initState() {
+    ReadData();
+    // TODO: implement initState
+    super.initState();
+    player.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isplaying = state == PlayerState.playing;
+      });
+    });
+    player.onDurationChanged.listen((newduration) {
+      duration = newduration;
+    });
+    player.onPositionChanged.listen((newposition) {
+      setState(() {
+        position = newposition;
+      });
+    });
+  }
+
+  Widget btnStart() {
+    return IconButton(
+        padding: EdgeInsets.only(bottom: 10),
+        onPressed: () {
+          setState(() {
+            if (isplaying) {
+              player.pause();
+            } else {
+              player.play(AssetSource(Books[i]["voice"]));
+            }
+          });
+        },
+        icon: Icon(
+          isplaying ? _icons[1] : _icons[0],
+          color: Colors.blue,
+          size: 50,
+        ));
+  }
+
+  Widget btn_next_one() {
+    return IconButton(
+        padding: EdgeInsets.only(bottom: 10),
+        onPressed: () {
+          setState(() {
+            i--;
+            if (i < 0) {
+              i = Books.length - 1;
+            }
+
+            player.play(AssetSource(Books[i]["voice"]));
+          });
+          secons = 0;
+          print(i);
+        },
+        icon: Icon(
+          Icons.skip_previous,
+          size: 40,
+        ));
+  }
+
+  Widget btn_previos_one() {
+    return IconButton(
+        padding: EdgeInsets.only(bottom: 10),
+        onPressed: () {
+          setState(() {
+            i++;
+            if (i > Books.length - 1) {
+              i = 0;
+            }
+          });
+          secons = 0;
+          player.play(AssetSource(Books[i]["voice"]));
+          print(i);
+        },
+        icon: Icon(
+          Icons.skip_next,
+          size: 40,
+        ));
+  }
+
+  Widget btn_repeoat() {
+    return IconButton(
+        onPressed: () {
+          if (isRepeat == false) {
+            player.setReleaseMode(ReleaseMode.loop);
+            setState(() {
+              isRepeat = true;
+              color = Colors.blue;
+            });
+          } else if (isRepeat == true) {
+            player.setReleaseMode(ReleaseMode.release);
+          }
+        },
+        icon: Icon(
+          Icons.repeat,
+          color: color,
+        ));
+  }
+
+  Widget btnLoop() {
+    return IconButton(
+        onPressed: () {
+          
+        },
+        icon: Icon(
+          Icons.loop,
+          color: ispaused ? Colors.blue : Colors.black,
+        ));
+  }
+
+  Widget loadAsset() {
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          btnLoop(),
+          btn_next_one(),
+          btnStart(),
+          btn_previos_one(),
+          btn_repeoat()
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [],
+            ),
+          ),
+          Slider(
+              min: secons,
+              activeColor: Colors.red,
+              max: duration.inSeconds.toDouble(),
+              value: position.inSeconds.toDouble(),
+              onChanged: (value) {
+                final position = Duration(seconds: value.toInt());
+                player.seek(position);
+                player.resume();
+              }),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(formatTime(position.inSeconds)),
+                Text(formatTime((position - duration).inSeconds)),
+              ],
+            ),
+          ),
+          loadAsset(),
+        ],
+      ),
+    );
+  }
+}
+// import 'package:audioplayers/audioplayers.dart';
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   return runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   final player = AudioPlayer();
+//   bool isplaying = false;
+//   Duration duration = Duration.zero;
+//   Duration position = Duration.zero;
+
+String formatTime(int second) {
+  return "${(Duration(seconds: second))}".split(".")[0].padLeft(8, "0");
+}
+
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     player.onPlayerStateChanged.listen((state) {
+//       setState(() {
+//         isplaying = state == PlayerState.playing;
+//       });
+//     });
+//     player.onDurationChanged.listen((newduration) {
+//       duration = newduration;
+//     });
+//     player.onPositionChanged.listen((newposition) {
+//       setState(() {
+//         position = newposition;
+//       });
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               CircleAvatar(
+//                 radius: 20,
+//                 child: IconButton(
+//                     onPressed: () {
+
+//                       if (isplaying) {
+//                         player.pause();
+//                       } else {
+//                         player
+//                             .play(AssetSource('milena_madmusayeva_-_taxi.mp3'));
+//                       }
+//                     },
+//                     icon: Icon(isplaying ? Icons.pause : Icons.play_arrow)),
+//               ),
+
+//               Slider(
+//                   min: 0,
+//                   max: duration.inSeconds.toDouble(),
+//                   value: position.inSeconds.toDouble(),
+//                   onChanged: (value) {
+//                     final position = Duration(seconds: value.toInt());
+//                     player.seek(position);
+//                     player.resume();
+//                   }),
+//               Container(
+//                 padding: EdgeInsets.all(20),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Text(formatTime(position.inSeconds)),
+//                     Text(formatTime((position - duration).inSeconds))
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/detail_audio.dart
+++ lib/detail_audio.dart
@@ -1,5 +1,10 @@
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:market/audio_detail.dart';
+
 import './app_colors.dart' as ApColors;
+
 class DetailAudio extends StatefulWidget {
   const DetailAudio({super.key});
 
@@ -8,35 +13,95 @@ class DetailAudio extends StatefulWidget {
 }
 
 class _DetailAudioState extends State<DetailAudio> {
+  late AudioPlayer advencedPlayer;
   @override
- 
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    advencedPlayer = AudioPlayer();
+  }
+
+  @override
   Widget build(BuildContext context) {
-   final double screenHeight = MediaQuery.of(context).size.height;
-   final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: ApColors.AudioBacround,
       body: Stack(
         children: [
           Positioned(
               top: 0,
               left: 0,
               right: 0,
-              height: screenHeight/3,
+              height: screenHeight / 3,
               child: Container(
                 height: 300,
-                color:ApColors.AudioBlueBacround ,
+                color: ApColors.AudioBlueBacround,
               )),
-              Positioned(
-                left: 0,
-                right: 0,
-                top: 0,
-                child: AppBar(
-                  backgroundColor: Colors.transparent,
-                  elevation: 0.0,
-                leading: IconButton(icon:Icon(Icons.arrow_back_ios),onPressed: (){}),
+          Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              child: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                leading: IconButton(
+                    icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
                 actions: [
-                  IconButton(onPressed: (){}, icon: Icon(Icons.search))
+                  IconButton(onPressed: () {}, icon: Icon(Icons.search)),
                 ],
-              ))
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              top: screenHeight * 0.2,
+              bottom: screenHeight * 0.36,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Colors.white),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: screenHeight * 0.1,
+                    ),
+                    Text(
+                      "THE WATER CURE",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Avenir"),
+                    ),
+                    Text(
+                      "Martin Hyatt",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    AudiaPlayer_Page()
+                  ],
+                ),
+              )),
+          Positioned(
+              top: screenHeight * 0.12,
+              left: (screenWidth - 150) / 2,
+              right: (screenWidth - 150) / 2,
+              height: screenHeight * 0.16,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: ApColors.AudioGreyBackround,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 2)),
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 5),
+                        image: DecorationImage(
+                            image: AssetImage("assets/music_vertical3.jpeg"),
+                            fit: BoxFit.cover)),
+                  ),
+                ),
+              )),
         ],
       ),
     );
